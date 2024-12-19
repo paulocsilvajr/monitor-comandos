d5a6cb053ebf67eed9a14db90ee13c03090afe8f
Paulo C. Silva Jr. <pauluscave@gmail.com>
2024-12-19 15:22:31 -0300

    Corrige tipo em função de helper de msg para teste

--- helper/msgErroTeste.go
+++ helper/msgErroTeste.go
@@ -5,5 +5,5 @@ import (
 )
 
 func MsgErroTeste(mensagem string, valorEsperado any, valorObtido any) string {
-	return fmt.Sprintf("%[1]s. Esperado: [%[2]s][%[2]T]. Obtido: [%[3]s][%[3]T]", mensagem, valorEsperado, valorObtido)
+	return fmt.Sprintf("%[1]s. Esperado: [%[2]v][%[2]T]. Obtido: [%[3]v][%[3]T]", mensagem, valorEsperado, valorObtido)
 }
