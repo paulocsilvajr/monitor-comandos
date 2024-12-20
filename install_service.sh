eb6caeb3a8bd0db1afc7c6ecfe7c73adb76583fe
Paulo C. Silva Jr. <pauluscave@gmail.com>
2024-12-20 12:58:13 -0300

    Adiciona validação de script install_service para criar serviço somente se existir o executável da API em pasta bin

--- install_service.sh
+++ install_service.sh
@@ -27,25 +27,30 @@ EOF
 
 # sudo ufw delete allow 8080
 case "$1" in
-    "-h" | "--help" )
+    "-h" | "--help")
+        echo "Script para registrar a API de '$DESCRICAO' como um serviço do Systemd"
         echo "$0 [-h | --help | -u | -r ]"
         echo -e "\t\t-h --help Exibe essa ajuda"
         echo -e "\t\t-u -r     Para e desativa o serviço '$SERVICO'"
         ;;
-    "-u" | "-r" )
+    "-u" | "-r")
         sudo systemctl stop "$SERVICO" &&
             sudo systemctl disable "$SERVICO" &&
             sudo ufw delete allow $PORT
         ;;
     *)
-        sudo ufw allow $PORT
+        if [ -f "${DIRETORIOBIN}/${PROGRAMA}" ]; then
+            sudo ufw allow $PORT
 
-        echo -e "\nCorpo do serviço '$SERVICO':"
-        echo -e "$servico" | sudo tee "$DESTINOSERVICO" &&
-            echo -e "\nSalvo serviço '$SERVICO' em '$DESTINO'\n" &&
-            sudo systemctl enable "$SERVICO"
+            echo -e "\nCorpo do serviço '$SERVICO':"
+            echo -e "$servico" | sudo tee "$DESTINOSERVICO" &&
+                echo -e "\nSalvo serviço '$SERVICO' em '$DESTINO'\n" &&
+                sudo systemctl enable "$SERVICO"
 
-        sudo systemctl start "$SERVICO"
-        sudo systemctl --no-pager -l status "$SERVICO"
+            sudo systemctl start "$SERVICO"
+            sudo systemctl --no-pager -l status "$SERVICO"
+        else
+            echo "Execute o script 'build.sh' para compilar a API no diretório 'bin'. Mais detalhes no README.md"
+        fi
         ;;
 esac
